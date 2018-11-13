e0962396fbdc14b631225045a27b5e6d2fd00048
Li Yachao <>
2018-11-13 18:03:20 +0900

    Updata DCGan

--- DCCGAN.py
+++ DCCGAN.py
@@ -5,6 +5,29 @@ from torch import nn
 from dataloader import CIFAR
 from torch.autograd import Variable
 import torchvision
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', required=True, help='cifar10 | lsun | imagenet | folder | lfw | fake')
+parser.add_argument('--dataroot', required=True, help='path to dataset')
+parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
+parser.add_argument('--batchsize', type=int, default=64, help='input batch size')
+parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
+parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
+parser.add_argument('--ngf', type=int, default=64)
+parser.add_argument('--ndf', type=int, default=64)
+parser.add_argument('--niter', type=int, default=25, help='number of epochs to train for')
+parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
+parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
+parser.add_argument('--cuda', action='store_true', help='enables cuda')
+parser.add_argument('--ngpu', type=int, default=1, help='number of GPUs to use')
+parser.add_argument('--netG', default='', help="path to netG (to continue training)")
+parser.add_argument('--netD', default='', help="path to netD (to continue training)")
+parser.add_argument('--outf', default='.', help='folder to output images and model checkpoints')
+parser.add_argument('--manualSeed', type=int, help='manual seed')
+
+opt = parser.parse_args()
+print(opt)
+
 class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
@@ -75,7 +98,7 @@ def main(epochs=100, bach_size=64 ):
 
 
 
-    data_loader = CIFAR(batch_size=bach_size)
+    data_loader = CIFAR(batch_size=opt.bachsize)
     criterion = nn.BCELoss().cuda()
     D_net = Discriminator().cuda()
     G_net = Generator().cuda()
